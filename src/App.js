ac1fa5414a7f00eb395c2b94d0a5c38793f8c26f
cferalfa <cferalfa@gmail.com>
2025-01-27 12:45:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Amplify }  from '@aws-amplify/core';
+import { Amplify } from '@aws-amplify/core';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsconfig from './aws-exports';
@@ -6,28 +6,45 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'semantic-ui-less/semantic.less';
 import Ccp from './components/ccp';
-//import { autoSignIn } from '@aws-amplify/auth';
 
 // Component
 function App({ signOut, user }) {
   const [isConfigured, setIsConfigured] = useState(false);
-  
+
   useEffect(() => {
     configureAuth();
-    //signedIn();
   }, []);
 
   const configureAuth = () => {
     Amplify.configure(awsconfig);
     setIsConfigured(true);
   };
-  //const signedIn = async () => {
-    //await autoSignIn();
-  //};
 
   return (
-    <div className="App">
-      {isConfigured && <Ccp user={user} signOut={signOut} />}
+    <div className="main-container">
+      {/* Left section for Agent Workspace */}
+      <div className="agent-workspace">
+        <iframe
+          src="https://synaptis-ai.my.connect.aws/agent-app-v2" // Replace with the correct Agent Workspace URL
+          title="Agent Workspace"
+          className="agent-workspace-iframe"
+        ></iframe>
+      </div>
+
+      {/* Right section for CCP and Translate */}
+      <div className="right-section">
+        {isConfigured && (
+          <>
+            {/* CCP container */}
+            <div id="ccp-container"></div>
+
+            {/* Translate Box */}
+            <div id="translate-box">
+              <Ccp user={user} signOut={signOut} />
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
